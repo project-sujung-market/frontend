aa1edd79cfaa01b91df9a0c46ab3c5d1a03abb76
Jiseop0831 <87520449+Jiseop0831@users.noreply.github.com>
2023-06-14 15:01:26 +0900

    upload-page

---
+++ Upload-page/Upload.js
@@ -0,0 +1,37 @@
+let modal = document.querySelector('.modal');
+
+let contents_name = document.querySelector('.contents-name');
+let contents_content = document.querySelector('.contents-content');
+let contents_address = document.querySelector('.contents-address');
+let contents_price = document.querySelector('.contents-price');
+let price = Number(contents_price.value);
+
+let modal_name = document.querySelector('.modal-inner-name');
+let modal_content = document.querySelector('.modal-inner-content');
+let modal_address = document.querySelector('.modal-inner-address');
+let modal_price = document.querySelector('.modal-inner-price');
+
+function open_modal() {
+    if(contents_name.value == "" || contents_content.value == "" || contents_address.value == "" || contents_price.value == "") {
+        alert('형식에 맞게 입력해주세요');
+    }
+    else {
+        modal.style.display = 'block';
+        modal_name.textContent = contents_name.value;
+        modal_content.textContent = contents_content.value;
+        modal_address.textContent = contents_address.value;
+        modal_price.textContent = `${Number(contents_price.value)}원`;
+    }
+}
+
+function close_modal() {
+    modal.style.display = 'none';
+}
+
+function formatPhoneNumber() {
+    const Num = document.querySelector(".contents-price");
+    const NumValue = Num.value;
+    const numbersOnly = NumValue.replace(/[^0-9]/g, "");
+  
+    Num.value = numbersOnly;
+}
